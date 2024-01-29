7ac2689b1529c17b7242f24041d466214c945e6f
Ricardo Bassete <ricardogbassete@gmail.com>
2024-01-29 17:33:21 -0300

    Add pages and templates

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+
+@section('content')
+	<div>
+		<h1>Terms</h1>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Id hic delectus velit, quod ducimus, laborum voluptas minus distinctio pariatur esse, aliquid laboriosam harum omnis. Hic dolores vitae similique modi enim!
+	</div>
+@endsection
