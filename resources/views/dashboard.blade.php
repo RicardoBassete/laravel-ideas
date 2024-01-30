6fe822ddab81f09993a9cb3ed927bf540aceb5d8
Ricardo Bassete <ricardogbassete@gmail.com>
2024-01-30 13:06:27 -0300

    Add link to terms page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
                 <span>Feed</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="{{route('terms')}}">
                 <span>Terms</span></a>
             </li>
             <li class="nav-item">
