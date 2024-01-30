88cb1de987688164dd46a1225f46e3f3c0617f88
Ricardo Bassete <ricardogbassete@gmail.com>
2024-01-30 13:27:22 -0300

    Add validation

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 class IdeaController extends Controller
 {
   public function store(Request $request) {
+
+		$request->validate([
+			'idea' => 'required|min:3|max:240'
+		]);
+
 		$idea = new Idea(['content' => $request->get('idea')]);
 		$idea->save();
 		return redirect()->route('dashboard')->with('success', 'Idea created Successfully!');
