3a4ee1942112849fafdf878a12a44a10d0c577f3
Ricardo Bassete <ricardogbassete@gmail.com>
2024-01-31 14:34:18 -0300

    Add register user method

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+	public function index() {
+		return view('auth.register');
+	}
+
+	public function create() {
+
+	}
+
+	public function store(Request $request) {
+		$validated = $request->validate([
+			'name' => 'required|min:3|max:40',
+			'email' => 'required|email|unique:users,email',
+			'password' => 'required|confirmed|min:8',
+		]);
+
+		User::create([
+			'name' => $validated['name'],
+			'email' => $validated['email'],
+			'password' => Hash::make($validated['name']),
+		]);
+
+		return redirect()->route('dashboard')->with('success', 'User created successfully!');
+	}
+
+	public function show() {
+
+	}
+
+	public function edit() {
+
+	}
+
+	public function update() {
+
+	}
+
+	public function destroy() {
+
+	}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
@@ -17,14 +18,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/terms', function(){ return view('terms'); })->name('terms');
-Route::get('/ideas/{id}', [IdeaController::class, 'show'])->name('ideas.show');
-Route::get('/ideas/{id}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
+Route::get('/', 										[DashboardController::class, 'index'])->name('dashboard');
 
-Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store');
+Route::get('/terms', 								function(){ return view('terms'); })->name('terms');
+
+Route::get('/ideas/{id}', 					[IdeaController::class, 'show'])->name('ideas.show');
+Route::get('/ideas/{id}/edit', 			[IdeaController::class, 'edit'])->name('ideas.edit');
+Route::post('/ideas', 							[IdeaController::class, 'store'])->name('ideas.store');
+Route::put('/ideas/{id}', 					[IdeaController::class, 'update'])->name('ideas.update');
+Route::delete('/ideas/{id}', 				[IdeaController::class, 'destroy'])->name('ideas.destroy');
+
 Route::post('/ideas/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
 
-Route::put('/ideas/{id}', [IdeaController::class, 'update'])->name('ideas.update');
-
-Route::delete('/ideas/{id}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+Route::get('/register', 						[AuthController::class, 'index'])->name('register');
+Route::post('/register', 						[AuthController::class, 'store'])->name('register.store');
