d6f4e7c1437f4c67e71e5cac7fd9c79465ffde0f
Ricardo Bassete <ricardogbassete@gmail.com>
2024-02-07 18:39:09 -0300

    update links

--- resources/views/includes/main-links.blade.php
+++ resources/views/includes/main-links.blade.php
@@ -3,28 +3,12 @@
     <div class="card-body pt-3">
       <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
         <li class="nav-item">
-          <a class="nav-link text-dark" href="{{ route('dashboard') }}">
+          <a class="nav-link {{ Route::is('dashboard') ? 'text-white bg-primary rounded' : '' }}" href="{{ route('dashboard') }}">
             <span>Home</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span>Explore</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span>Feed</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('terms') }}">
+          <a class="nav-link {{ Route::is('terms') ? 'text-white bg-primary rounded' : '' }}" href="{{ route('terms') }}">
             <span>Terms</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span>Support</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span>Settings</span></a>
         </li>
       </ul>
     </div>
--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,8 +1,23 @@
 @extends('layout.layout')
 
 @section('content')
-	<div>
-		<h1>Terms</h1>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Id hic delectus velit, quod ducimus, laborum voluptas minus distinctio pariatur esse, aliquid laboriosam harum omnis. Hic dolores vitae similique modi enim!
-	</div>
+  <div class="row">
+
+		@include('includes.main-links')
+
+    <div class="col-6">
+			<div>
+				<h1>Terms</h1>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Id hic delectus velit, quod ducimus, laborum voluptas minus distinctio pariatur esse, aliquid laboriosam harum omnis. Hic dolores vitae similique modi enim!
+			</div>
+    </div>
+    <div class="col-3">
+
+			@include('includes.search-bar')
+
+      @include('includes.who-to-follow')
+
+    </div>
+  </div>
 @endsection
+
